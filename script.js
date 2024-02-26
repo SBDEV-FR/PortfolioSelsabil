d493f713e083fb7996091777086a61da8961fed3
SBDEV-FR <115069416+SBDEV-FR@users.noreply.github.com>
2024-02-26 17:48:51 +0100

    Images corrigées, ajout de fenêtres popup (contenu à réviser

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("openPopupBtn").addEventListener("click", openPopup);
+
+function openPopup() {
+    document.getElementById("popupContainer").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popupContainer").style.display = "none";
+}
